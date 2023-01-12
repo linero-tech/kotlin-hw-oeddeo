21c29b7e4db8457dbba38fa97f8a03cfb3e455cd
oeddeo <104972668+oeddeo@users.noreply.github.com>
2023-01-12 10:41:41 +0100

    Update Task3.kt

--- src/main/kotlin/lms_126/Task3.kt
+++ src/main/kotlin/lms_126/Task3.kt
@@ -2,5 +2,9 @@ package lms_126
 
 fun task3(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        input: height, width
+        area: height * width * 0.5
+        output: area 
+    """
 }
