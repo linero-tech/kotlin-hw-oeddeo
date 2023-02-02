cbe528f368cc9788fbf198bbb754169d52b2e9f2
oeddeo <104972668+oeddeo@users.noreply.github.com>
2023-02-02 22:08:24 +0100

    Update Task10.kt

--- src/main/kotlin/lms_129/Task10.kt
+++ src/main/kotlin/lms_129/Task10.kt
@@ -2,5 +2,23 @@ package lms_129
 
 fun task10(password: String): Boolean {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val regReq2 = "^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[#@$]).{4,10}$"
+    val requiredList = listOf("[a-z]", "[A-Z]", "[0-9]", "[#@$]", "^.{0,10}$", "^.{4,}$")
+    val result = password.contains(Regex(regReq2))
+    if (!result)
+        for(each in requiredList){
+        if(!password.contains(Regex(each))) {
+            print("missing $each --> ")
+        }
+        }
+    return result
+    }
+
+fun main() {
+    val pwList = listOf("aCa9ab", "AAAaabcAAA239180ewqdioj1#", "psW9$", "aBcD#$", "$1a")
+    for(pw in pwList){
+    val passed = task10(password = pw)
+        println(passed)
+
+        }
 }
