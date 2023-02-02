cfbdb76db3082a97f09f139041e793475a7cb31a
oeddeo <104972668+oeddeo@users.noreply.github.com>
2023-02-02 10:46:15 +0100

    Update Task2.kt

--- src/main/kotlin/lms_129/Task2.kt
+++ src/main/kotlin/lms_129/Task2.kt
@@ -1,15 +1,17 @@
 package lms_129
 
 fun task2(number: Int): Boolean {
-    var result = number > 1
-    for(num in 2 until number){
-        if(number % num == 0){
-             false
+    var result = true
+    if(number > 1) {
+        for(num in 2 until number ) {
+            if (number % num == 0) {
+                result = false
+            }
         }
-    }
+    }else result = false
     return result
 }
 fun main() {
-   val answer = task2(number = 11)
+   val answer = task2(number = 66)
     println(answer)
 }
