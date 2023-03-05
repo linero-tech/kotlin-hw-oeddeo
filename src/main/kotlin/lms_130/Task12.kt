b1ba11050cc988c40dbb23fd4c653df16fe6ec2b
oeddeo <104972668+oeddeo@users.noreply.github.com>
2023-03-05 10:05:53 +0100

    Update Task12.kt

--- src/main/kotlin/lms_130/Task12.kt
+++ src/main/kotlin/lms_130/Task12.kt
@@ -1,8 +1,8 @@
 package lms_130
 
-fun task12_1(sells: List<Int>, days: List<String>): String {
+fun task12_1(sells: List<Int>): String {
     // most sells:
-
+    val days = listOf("Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
     var max = 0
     var result = ""
     sells.forEachIndexed { index, i -> if(i > max) {
@@ -12,7 +12,8 @@ fun task12_1(sells: List<Int>, days: List<String>): String {
    return result
 }
 
-fun task12_2(sells: List<Int>, days: List<String>): String {
+fun task12_2(sells: List<Int>): String {
+    val days = listOf("Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
     var min = if(sells.isNotEmpty()) {sells[0]} else {0}
     var result = ""
     sells.forEachIndexed { index, i ->  if(i < min) {
@@ -30,7 +31,7 @@ fun task12_3(sells: List<Int>): Int {
 }
 fun main() {
     val days = listOf("Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
-    println(task12_1(listOf(20000, 1000, 800, 300, 768, 765, 9), days))
-    println(task12_2(listOf(20, 100, 800, 768, 765, 90, 100), days))
+    println(task12_1(listOf(20000, 1000, 800, 300, 768, 765, 9)))
+    println(task12_2(listOf(20, 100, 800, 768, 765, 90, 100)))
     println(task12_3(listOf(5, 10, 15, 20, 25, 30, 35)))
 }
