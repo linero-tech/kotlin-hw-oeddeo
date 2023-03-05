fc13a9afac8e85ec8f8c9074fca84f124b3c2ff2
oeddeo <104972668+oeddeo@users.noreply.github.com>
2023-03-05 10:31:54 +0100

    mod4, task12

--- src/main/kotlin/lms_130/Task12.kt
+++ src/main/kotlin/lms_130/Task12.kt
@@ -19,7 +19,7 @@ fun task12_2(sells: List<Int>): String {
     sells.forEachIndexed { index, i ->  if(i < min) {
         min = i
         result = days[index]} }
-    print("min is $result")
+
     return result
 }
 
@@ -30,7 +30,6 @@ fun task12_3(sells: List<Int>): Int {
     return result
 }
 fun main() {
-    val days = listOf("Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
     println(task12_1(listOf(20000, 1000, 800, 300, 768, 765, 9)))
     println(task12_2(listOf(20, 100, 800, 768, 765, 90, 100)))
     println(task12_3(listOf(5, 10, 15, 20, 25, 30, 35)))
