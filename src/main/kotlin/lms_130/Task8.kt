294e34ec1e2e67ca2147cee810c8b2806af9f444
oeddeo <104972668+oeddeo@users.noreply.github.com>
2023-03-09 11:22:13 +0100

    original:(

--- src/main/kotlin/lms_130/Task8.kt
+++ src/main/kotlin/lms_130/Task8.kt
@@ -3,18 +3,37 @@ package lms_130
 
 fun task8(items: List<Int>): List<Int> {
 
-    val values = mutableListOf<Int>()
-    val step1 = items.toSet().sorted()
-    for (index in (step1.size - 3) until step1.size) {
-        values.add(step1[index])
+   var top3 = mutableListOf(items[0], items[1], items[2]).sorted().toMutableList()
+    items.forEach { item ->
+        if (item > top3[0] && item !in top3) {
+            top3[0] = item
+            top3 = top3.sorted().toMutableList()
+        }
     }
+    return top3.toSet().toList()
 
-    return values
+    /*
+    Alt 1:
+    val hitList1 = mutableListOf(10, 10, 10, 10, 10, 10, 10, 1, 100)
+    var topHits = mutableListOf<Int>()
+    hitList1.forEach { nr -> if (topHits.size < 3 && nr !in topHits) {
+        topHits.add(nr)
+    }else {
+        if(nr > topHits.sorted()[0]) {
+            topHits[0] = nr
+            topHits = topHits.sorted().toMutableList()
+        }
+
+    } }
+    println(topHits.sorted())
+
+     */
+
 
 
 }
 
 
 fun main() {
-    print(task8(listOf(90, 100, 1, 900, 100, 50, 32, 999, -100)))
+    print(task8(listOf(1, 10, 9, 100, 50, 30, 20, 90, 200, 0, 5)))
 }
