5971a7c6312a5dfd4c6643dbb3599cf1711c0747
oeddeo <104972668+oeddeo@users.noreply.github.com>
2023-03-09 11:11:26 +0100

    Update Task8.kt

--- src/main/kotlin/lms_130/Task8.kt
+++ src/main/kotlin/lms_130/Task8.kt
@@ -2,36 +2,17 @@ package lms_130
 
 
 fun task8(items: List<Int>): List<Int> {
-    val top3 = mutableListOf<Int>()
-    val singles = items.toSet().toList()
 
-    for (item in singles) {
-        if (top3.size < 3) {
-            top3.add(item)
-        } else {
-            if (item > top3[0]) {
-                top3[0] = item
-                top3.sort()
-            }
-        }
+    val values = mutableListOf<Int>()
+    val step1 = items.toSet().sorted()
+    for (index in (step1.size - 3) until step1.size) {
+        values.add(step1[index])
     }
 
-    return top3
+    return values
 
 
-        /* Poop
-        val values = mutableListOf<Int>()
-        val step1 = items.toSet().sorted()
-        for (index in (step1.size - 3) until step1.size) {
-            values.add(step1[index])
-        }
-
-        return values
-
-    val top = items.toSet().toList().sortedDescending().take(3).sorted()
-    return top
-    */
-    }
+}
 
 
 fun main() {
